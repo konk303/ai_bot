b4f87513c6d79e16965bed87787b5f926b39b4b4
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2025-05-04 23:02:08 +0900

    Make it work

--- agent/deploy.py
+++ agent/deploy.py
@@ -3,38 +3,120 @@ import vertexai
 from dotenv import load_dotenv
 from vertexai.preview import reasoning_engines
 from vertexai import agent_engines
-from agent import root_agent
+# from agent.agent import root_agent
 
-load_dotenv()
-PROJECT_ID = os.getenv("GOOGLE_CLOUD_PROJECT")
-LOCATION = os.getenv("GOOGLE_CLOUD_LOCATION")
+# load_dotenv()
+# PROJECT_ID = os.getenv("GOOGLE_CLOUD_PROJECT")
+# LOCATION = os.getenv("GOOGLE_CLOUD_LOCATION")
 STAGING_BUCKET = "gs://ai-agent-staging"
 
+
+
+
+
+import datetime
+from zoneinfo import ZoneInfo
+from google.adk.agents import Agent
+
+def get_weather(city: str) -> dict:
+    """Retrieves the current weather report for a specified city.
+
+    Args:
+        city (str): The name of the city for which to retrieve the weather report.
+
+    Returns:
+        dict: status and result or error msg.
+    """
+    if city.lower() == "new york":
+        return {
+            "status": "success",
+            "report": (
+                "The weather in New York is sunny with a temperature of 25 degrees"
+                " Celsius (77 degrees Fahrenheit)."
+            ),
+        }
+    else:
+        return {
+            "status": "error",
+            "error_message": f"Weather information for '{city}' is not available.",
+        }
+
+
+def get_current_time(city: str) -> dict:
+    """Returns the current time in a specified city.
+
+    Args:
+        city (str): The name of the city for which to retrieve the current time.
+
+    Returns:
+        dict: status and result or error msg.
+    """
+
+    if city.lower() == "new york":
+        tz_identifier = "America/New_York"
+    else:
+        return {
+            "status": "error",
+            "error_message": (
+                f"Sorry, I don't have timezone information for {city}."
+            ),
+        }
+
+    tz = ZoneInfo(tz_identifier)
+    now = datetime.datetime.now(tz)
+    report = (
+        f'The current time in {city} is {now.strftime("%Y-%m-%d %H:%M:%S %Z%z")}'
+    )
+    return {"status": "success", "report": report}
+
+
+root_agent = Agent(
+    name="weather_time_agent",
+    model="gemini-2.0-flash",
+    description=(
+        "Agent to answer questions about the time and weather in a city."
+    ),
+    instruction=(
+        "You are a helpful agent who can answer user questions about the time and weather in a city."
+    ),
+    tools=[get_weather, get_current_time],
+)
+
+
+
+
+
+
+
 vertexai.init(
-    project=PROJECT_ID,
-    location=LOCATION,
+    project="develop-458412",
+    location="us-central1",
     staging_bucket=STAGING_BUCKET,
 )
 
-# app = reasoning_engines.AdkApp(
-#     agent=root_agent,
-#     enable_tracing=True,
+app = reasoning_engines.AdkApp(
+    agent=root_agent,
+    enable_tracing=True,
+)
+
+session = app.create_session(user_id="u_123")
+print(session)
+print(app.list_sessions(user_id="u_123"))
+
+for event in app.stream_query(
+    user_id="u_123",
+    session_id=session.id,
+    message="whats the weather in new york",
+):
+    print(event)
+
+# remote_app = agent_engines.create(
+#     display_name="city_time_agent",
+#     agent_engine=root_agent,
+#     requirements=[
+#         "google-cloud-aiplatform[adk,agent_engines]"
+#     ]
 # )
 
-# session = app.create_session(user_id="u_123")
-# print(session)
-# print(app.list_sessions(user_id="u_123"))
 
-# for event in app.stream_query(
-#     user_id="u_123",
-#     session_id=session.id,
-#     message="whats the weather in new york",
-# ):
-#     print(event)
-
-remote_app = agent_engines.create(
-    agent_engine=root_agent,
-    requirements=[
-        "google-cloud-aiplatform[adk,agent_engines]"
-    ]
-)
+print(reasoning_engines.ReasoningEngine.list())
