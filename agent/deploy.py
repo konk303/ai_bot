7d7c1b9feb46234c1b517ec2344123dd1f658348
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2025-05-04 11:57:14 +0900

    Add gcs bucket

--- agent/deploy.py
+++ agent/deploy.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 PROJECT_ID = os.getenv("GOOGLE_CLOUD_PROJECT")
-LOCATION = "us-central1"
-STAGING_BUCKET = "gs://your-google-cloud-storage-bucket"
+LOCATION = os.getenv("GOOGLE_CLOUD_LOCATION")
+STAGING_BUCKET = "gs://ai-agent-staging"
 
 vertexai.init(
     project=PROJECT_ID,
