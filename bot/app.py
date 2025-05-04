efc8cc7b285b659ca580aaa58ae51405378ef8d2
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2025-05-04 23:40:15 +0900

    Bot

--- bot/app.py
+++ bot/app.py
@@ -1,6 +1,10 @@
 import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from vertexai import agent_engines
+
+agent_engine = agent_engines.get('projects/69263171180/locations/us-central1/reasoningEngines/6920422942245388288')
+remote_session = agent_engine.create_session(user_id="u_456")
 
 # ボットトークンを渡してアプリを初期化します
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
@@ -32,6 +36,14 @@ def action_button_click(body, ack, say):
     ack()
     # チャンネルにメッセージを投稿します
     say(f"<@{body['user']['id']}> さんがボタンをクリックしました！")
+    for event in agent_engine.stream_query(
+        user_id="u_456",
+        session_id=remote_session["id"],
+        message="whats the weather in new york",
+    ):
+        result = event['content']['parts'][0]
+        if 'text' in result:
+            say(result['text'])
 
 if __name__ == "__main__":
     # アプリを起動して、ソケットモードで Slack に接続します
