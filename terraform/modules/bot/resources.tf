117095668447e3553a5068c329e558d298492f60
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2025-05-23 16:58:09 +0900

    Set min_instance

--- terraform/modules/bot/resources.tf
+++ terraform/modules/bot/resources.tf
@@ -163,6 +163,10 @@ resource "google_cloud_run_v2_service" "ai-bot" {
         }
       }
     }
+    scaling {
+      min_instance_count = 1
+      max_instance_count = 40
+    }
   }
 
   lifecycle {
