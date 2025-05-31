c99a132c2120c840a640d6bfbfab43e01d380f26
Kimihiko NAKAMURA <nakamura.808@gmail.com>
2025-05-31 16:22:39 +0900

    messing with deploy

--- agent/deploy.py
+++ agent/deploy.py
@@ -4,7 +4,7 @@ import vertexai
 from dotenv import load_dotenv
 from vertexai import agent_engines
 from vertexai.preview import reasoning_engines
-from root_agent.create_agent import create_agent
+from root_agent.agent import root_agent
 
 DISPLAY_NAME = "gemini_agent"
 
@@ -13,76 +13,68 @@ PROJECT_ID = os.getenv("GOOGLE_CLOUD_PROJECT")
 LOCATION = os.getenv("GOOGLE_CLOUD_LOCATION")
 STAGING_BUCKET = "gs://ai-agent-staging"
 
-async def main():
-    vertexai.init(
-        project=PROJECT_ID,
-        location=LOCATION,
-        staging_bucket=STAGING_BUCKET,
-    )
+vertexai.init(
+    project=PROJECT_ID,
+    location=LOCATION,
+    staging_bucket=STAGING_BUCKET,
+)
 
-    # # Local testing
-    # local_test_agent, local_test_exit_stack = await create_agent()
-    # async with local_test_exit_stack:
-    #     app = reasoning_engines.AdkApp(
-    #         agent=local_test_agent,
-    #         enable_tracing=True,
-    #     )
-    #     for event in app.stream_query(
-    #             user_id="u_123",
-    #             message="whats the weather in new york",
-    #     ):
-    #         print(event)
+# Local testing
+# app = reasoning_engines.AdkApp(
+#     agent=root_agent,
+#     enable_tracing=True,
+# )
 
-    remote_apps = list(agent_engines.list(filter=f'display_name="{DISPLAY_NAME}"'))
-
-    if len(remote_apps) > 1:
-        for remote_app in remote_apps[1:]:
-            print("delete old agent engine")
-            remote_app.delete(force=True)
-
-    agent, _ = await create_agent()
-
-    if len(remote_apps) == 0:
-        print("create new agent engine")
-        remote_app = agent_engines.create(
-            display_name=DISPLAY_NAME,
-            agent_engine=agent,
-            requirements=[
-                "google-cloud-aiplatform[adk,agent_engines]"
-            ],
-            extra_packages=[
-                "root_agent",
-                "map",
-                "gdrive",
-                "google_search",
-            ]
-        )
-    else:
-        print("update existing agent engine")
-        remote_app = remote_apps[0].update(
-            display_name=DISPLAY_NAME,
-            agent_engine=agent,
-            requirements=[
-                "google-cloud-aiplatform[adk,agent_engines]"
-            ],
-            extra_packages=[
-                "root_agent",
-                "map",
-                "gdrive",
-                "google_search",
-            ]
-        )
-
-# print(remote_app)
-# for event in remote_app.stream_query(
-#         user_id="test_user",
-#         message="Hello, how are you?",
+# for event in app.stream_query(
+#     user_id="u_123",
+#     message="whats the weather in new york?",
 # ):
 #     print(event)
 
-# for agent_engine in agent_engines.list():
-#     print(f"{agent_engine.display_name}: {agent_engine.resource_name}")
-#     # agent_engine.delete(force=True)
+remote_apps = list(agent_engines.list(filter=f'display_name="{DISPLAY_NAME}"'))
 
-if __name__ == "__main__":
-    asyncio.run(main())
+if len(remote_apps) > 1:
+    for remote_app in remote_apps[1:]:
+        print("delete old agent engine")
+        remote_app.delete(force=True)
+
+
+if len(remote_apps) == 0:
+    print("create new agent engine")
+    remote_app = agent_engines.create(
+        display_name=DISPLAY_NAME,
+        agent_engine=root_agent,
+        requirements=[
+            "google-cloud-aiplatform[adk,agent_engines]"
+        ],
+        extra_packages=[
+            "root_agent",
+            "map",
+            "google_search",
+        ]
+    )
+else:
+    print("update existing agent engine")
+    remote_app = remote_apps[0].update(
+        display_name=DISPLAY_NAME,
+        agent_engine=root_agent,
+        requirements=[
+            "google-cloud-aiplatform[adk,agent_engines]"
+        ],
+        extra_packages=[
+            "root_agent",
+            "map",
+            "google_search",
+        ]
+    )
+
+print(remote_app)
+for event in remote_app.stream_query(
+        user_id="test_user",
+        message="Hello, how are you?",
+):
+    print(event)
+
+for agent_engine in agent_engines.list():
+    print(f"{agent_engine.display_name}: {agent_engine.resource_name}")
+    # agent_engine.delete(force=True)
